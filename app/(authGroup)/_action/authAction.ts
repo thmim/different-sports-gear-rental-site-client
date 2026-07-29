de0c39450be0db98f018b6e8ebf2440dd46a8a7c
thmim <mdtaharimmim1010@gmail.com>
2026-07-29 19:45:15 +0600

    feat:added user login functionality

--- app/(authGroup)/_action/authAction.ts
+++ app/(authGroup)/_action/authAction.ts
@@ -1,4 +1,7 @@
 "use server"
+
+import { cookies } from "next/headers"
+
 type loginState = {
   success:boolean,
   statusCode:number,
@@ -10,7 +13,7 @@ type loginState = {
 }
 
 export const loginAction = async (previousState:loginState,formData:FormData) =>{
-    console.log(previousState,"prev")
+    // console.log(previousState,"prev")
 
 const email = formData.get("email")
 const password = formData.get("password")
@@ -29,10 +32,26 @@ const res = await fetch(`${process.env.BACKEND_API_URL}/api/auth/login`,{
 })
 const result = await res.json();
 console.log(result)
-if(!result.success){
-    // console.log("parla na")
-    // showing user not found toast here
-}
+
+if(result.success){
+    const cookieStore = await cookies();
+    // set access token
+  cookieStore.set("accessToken",result.data.accessToken,{
+    httpOnly:true,
+    maxAge:60* 60 * 24, //1 day
+    sameSite:"lax"
+  })
+
+  // set refresh token
+  cookieStore.set("refreshToken",result.data.refreshToken,{
+    httpOnly:true,
+    maxAge:60* 60 * 7 * 24, //7 day
+    sameSite:"lax"
+  })
+
+  // redirect("/dashboard","push");  // redirect users desire page after successful login
+  
+  }
  return result;
  
 }
